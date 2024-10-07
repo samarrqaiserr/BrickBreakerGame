1f781b6533b1b07bd253bc82c6e9ba76bcbcb33b
Samar Qaiser <144149918+samarrqaiserr@users.noreply.github.com>
2024-10-07 18:48:18 +0500

    Add files via upload

---
+++ Main_class.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author samarqaiser
+ */
+package BrickBreakerGame;
+import javax.swing.JFrame;
+public class Main_class {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay game = new Gameplay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BRICK BREAKER");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(game);
+    }
+
+}
